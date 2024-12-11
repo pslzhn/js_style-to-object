3d15d05d850eab02df2b3ecc1927d85261b7d25b
Vlad Poseliuzhnyi <poseliuzhnyiv@gmail.com>
2024-12-10 19:02:25 -0800

    add solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,23 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const styles = {};
+
+  sourceString
+    .split('\n')
+    .map((line) => line.trim())
+    .filter((line) => line)
+    .join(' ')
+    .split(';')
+    .forEach((declaration) => {
+      const [key, value] = declaration.split(/:(.+)/);
+
+      if (key && value) {
+        styles[key.trim()] = value.trim();
+      }
+    });
+
+  return styles;
 }
 
 module.exports = convertToObject;
